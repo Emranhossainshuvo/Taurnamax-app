3cb806b5415bd17ce6678b309a5fc14f57ba7da2
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-15 07:49:19 +0600

    functionality added to send topics to the backend from the frontend

--- src/app/addTopic/page.jsx
+++ src/app/addTopic/page.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+"use client"
+import { useEffect } from 'react';
 import Navbar from '../../components/Navbar';
 
-const addTopic = () => {
+const AddTopic = () => {
+
+    
+
+        const handleAddTopic = async (e) => {
+            e.preventDefault();
+            const form = e.target;
+            const title = form.title.value;
+            const description = form.description.value;
+    
+            const topic = {
+                title,
+                description
+            };
+    
+            // Send data to the backend
+            const response = await fetch('http://localhost:5000/topics', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(topic),
+            });
+    
+            const data = await response.json();
+            console.log('Response from backend:', data);
+        };
+    
+
     return (
         <div className="max-w-screen-md mx-auto">
             <Navbar />
-            <input type="text" placeholder='Topic title' className='border-gray-500 border-[1px] w-full mb-3 p-2' />
-            <input type="text" placeholder='Topic Descriptin' className='border-gray-500 border-[1px] w-full p-2' />
-            <button className='text-white text-xl font-semibold bg-[#16A34A] hover:bg-[#16a34ab9] rounded-sm p-2 mt-3'>Add topic</button>
+            <form onSubmit={handleAddTopic}>
+                <input name='title' type="text" placeholder='Topic title' className='border-gray-500 border-[1px] w-full mb-3 p-2' />
+                <input type="text" name='description'  placeholder='Topic Descriptin' className='border-gray-500 border-[1px] w-full p-2' />
+                <button type='submit' className='text-white text-xl font-semibold bg-[#16A34A] hover:bg-[#16a34ab9] rounded-sm p-2 mt-3'>Add topic</button>
+            </form>
         </div>
     );
 };
 
-export default addTopic;
+export default AddTopic;
